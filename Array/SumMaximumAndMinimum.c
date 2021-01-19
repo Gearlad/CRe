3756fa5cf0dc66fbcb30e63f9bfd09c8d269cec0
Seth Austin Harding <gearladeldsvon@gmail.com>
2021-01-19 16:03:28 +0800

    Create SumMaximumAndMinimum.c

---
+++ Array/SumMaximumAndMinimum.c
@@ -0,0 +1,34 @@
+ 
+main() {
+    int N, m;
+ 
+    scanf("%d%d", &N, &m);
+ 
+    int arr[N];
+ 
+    for(int i = 0; i < N; i++) {
+        scanf("%d", &arr[i]);
+    }
+ 
+    /*for(int i = 0; i < N; i++) {
+        printf("%d\n", arr[i]);
+    }*/
+ 
+    int sum, max, min;
+ 
+    for(int i = 0; i < m; i++) {
+        sum = 0;
+        max = 0;
+        min = 10000;
+        for(int j = 0; j < N; j++) {
+            if(arr[j] % m == i) {
+                sum += arr[j];
+                if(arr[j] > max) max = arr[j];
+                if(arr[j] < min) min = arr[j];
+            }
+        }
+        printf("%d %d %d\n", sum, max, min);
+    }
+ 
+    return 0;
+}
